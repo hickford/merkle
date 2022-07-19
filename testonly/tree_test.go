97a7536d44a47166e0ea7f773559eff88a9d59f9
hickford <hickford@google.com>
2022-07-19 11:16:18 +0100

    Make existing tests into fuzz tests where possible
    Add seed inputs

--- testonly/tree_test.go
+++ testonly/tree_test.go
@@ -17,7 +17,6 @@ package testonly
 import (
 	"bytes"
 	"fmt"
-	"math/rand"
 	"strconv"
 	"testing"
 
@@ -67,49 +66,50 @@ func TestBuildTreeBuildAllAtOnce(t *testing.T) {
 	validateTree(t, mt, 8)
 }
 
-func TestTreeHashAt(t *testing.T) {
-	test := func(desc string, entries [][]byte) {
-		t.Run(desc, func(t *testing.T) {
-			mt := newTree(entries)
-			for size := 0; size <= len(entries); size++ {
-				got := mt.HashAt(uint64(size))
-				want := refRootHash(entries[:size], mt.hasher)
-				if !bytes.Equal(got, want) {
-					t.Errorf("HashAt(%d): %x, want %x", size, got, want)
-				}
-			}
-		})
+func FuzzHashAtAgainstReferenceImplementation(f *testing.F) {
+	for size := 0; size <= 8; size++ {
+		for index := 0; index <= size; index++ {
+			f.Add(uint64(index), uint64(size))
+		}
 	}
-
-	entries := LeafInputs()
-	for size := 0; size <= len(entries); size++ {
-		test(fmt.Sprintf("size:%d", size), entries[:size])
-	}
-	test("generated", genEntries(256))
+	f.Fuzz(func(t *testing.T, index, size uint64) {
+		t.Logf("index=%d, size=%d", index, size)
+		if index >= size {
+			return
+		}
+		entries := genEntries(size)
+		mt := newTree(entries)
+		got := mt.HashAt(uint64(size))
+		want := refRootHash(entries[:size], mt.hasher)
+		if !bytes.Equal(got, want) {
+			t.Errorf("HashAt(%d): %x, want %x", size, got, want)
+		}
+	})
 }
 
-func TestTreeInclusionProof(t *testing.T) {
-	test := func(desc string, entries [][]byte) {
-		t.Run(desc, func(t *testing.T) {
-			mt := newTree(entries)
-			for index, size := uint64(0), uint64(len(entries)); index < size; index++ {
-				got, err := mt.InclusionProof(index, size)
-				if err != nil {
-					t.Fatalf("InclusionProof(%d, %d): %v", index, size, err)
-				}
-				want := refInclusionProof(entries[:size], index, mt.hasher)
-				if diff := cmp.Diff(got, want, cmpopts.EquateEmpty()); diff != "" {
-					t.Fatalf("InclusionProof(%d, %d): diff (-got +want)\n%s", index, size, diff)
-				}
-			}
-		})
+func FuzzInclusionProofAgainstReferenceImplementation(f *testing.F) {
+	for size := 0; size <= 8; size++ {
+		for index := 0; index <= size; index++ {
+			f.Add(uint64(index), uint64(size))
+		}
 	}
-
-	test("generated", genEntries(256))
-	entries := LeafInputs()
-	for size := 0; size < len(entries); size++ {
-		test(fmt.Sprintf("golden:%d", size), entries[:size])
-	}
+	f.Fuzz(func(t *testing.T, index, size uint64) {
+		t.Logf("index=%d, size=%d", index, size)
+		if index >= size {
+			return
+		}
+		entries := genEntries(size)
+		tree := newTree(entries)
+		got, err := tree.InclusionProof(index, size)
+		t.Logf("proof=%v", got)
+		if err != nil {
+			t.Error(err)
+		}
+		want := refInclusionProof(entries, index, tree.hasher)
+		if diff := cmp.Diff(got, want, cmpopts.EquateEmpty()); diff != "" {
+			t.Fatalf("InclusionProof(%d, %d): diff (-got +want)\n%s", index, size, diff)
+		}
+	})
 }
 
 func TestTreeConsistencyProof(t *testing.T) {
@@ -137,29 +137,41 @@ func TestTreeConsistencyProof(t *testing.T) {
 	}
 }
 
-// Make random proof queries and check against the reference implementation.
-func TestTreeConsistencyProofFuzz(t *testing.T) {
-	entries := genEntries(256)
-
-	for treeSize := int64(1); treeSize <= 256; treeSize++ {
-		mt := newTree(entries[:treeSize])
-		for i := 0; i < 8; i++ {
-			size2 := uint64(rand.Int63n(treeSize + 1))
-			size1 := uint64(rand.Int63n(int64(size2) + 1))
-
-			got, err := mt.ConsistencyProof(size1, size2)
-			if err != nil {
-				t.Fatalf("ConsistencyProof: %v", err)
-			}
-			want := refConsistencyProof(entries[:size2], size2, size1, mt.hasher, true)
-			if diff := cmp.Diff(got, want, cmpopts.EquateEmpty()); diff != "" {
-				t.Errorf("ConsistencyProof: diff (-got +want)\n%s", diff)
+// Make random proof queries and check the reference implementation.
+func FuzzConsistencyProofAgainstReferenceImplementation(f *testing.F) {
+	for size := 0; size <= 8; size++ {
+		for size2 := 0; size2 <= size; size2++ {
+			for size1 := 0; size1 <= size2; size1++ {
+				f.Add(uint64(size), uint64(size1), uint64(size2))
 			}
 		}
 	}
+	f.Fuzz(func(t *testing.T, size, size1, size2 uint64) {
+		t.Logf("size=%d, size1=%d, size2=%d", size, size1, size2)
+		if size1 > size2 || size2 > size {
+			return
+		}
+		entries := genEntries(size)
+		tree := newTree(entries)
+		got, err := tree.ConsistencyProof(size1, size2)
+		if err != nil {
+			t.Fatalf("ConsistencyProof: %v", err)
+		}
+		want := refConsistencyProof(entries, size2, size1, tree.hasher, true)
+		if diff := cmp.Diff(got, want, cmpopts.EquateEmpty()); diff != "" {
+			t.Errorf("ConsistencyProof: diff (-got +want)\n%s", diff)
+		}
+	})
 }
 
 func FuzzConsistencyProof(f *testing.F) {
+	for size := 0; size <= 8; size++ {
+		for size2 := 0; size2 <= size; size2++ {
+			for size1 := 0; size1 <= size2; size1++ {
+				f.Add(uint64(size), uint64(size1), uint64(size2))
+			}
+		}
+	}
 	f.Fuzz(func(t *testing.T, size, size1, size2 uint64) {
 		t.Logf("size=%d, size1=%d, size2=%d", size, size1, size2)
 		if size1 > size2 || size2 > size {
@@ -179,6 +191,11 @@ func FuzzConsistencyProof(f *testing.F) {
 }
 
 func FuzzInclusionProof(f *testing.F) {
+	for size := 0; size <= 8; size++ {
+		for index := 0; index <= size; index++ {
+			f.Add(uint64(index), uint64(size))
+		}
+	}
 	f.Fuzz(func(t *testing.T, index, size uint64) {
 		t.Logf("index=%d, size=%d", index, size)
 		if index >= size {
