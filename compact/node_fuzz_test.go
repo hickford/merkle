69c20ea4c7162ea3700a07e4f1e51e70fa0e54fc
hickford <hickford@google.com>
2022-07-22 16:27:50 +0100

    Sabotage

--- compact/node_fuzz_test.go
+++ compact/node_fuzz_test.go
@@ -10,24 +10,8 @@ import (
 // https://github.com/transparency-dev/merkle/blob/main/docs/compact_ranges.md#definition
 func FuzzRangeNodes(f *testing.F) {
 	f.Fuzz(func(t *testing.T, begin, end uint64) {
-		if begin > end {
-			return
-		}
-		t.Logf("begin=%d, end=%d", begin, end)
-		nodes := RangeNodes(begin%200, end, nil)
-		t.Logf("nodes=%v", nodes)
-
-		// Nodes should be contiguous covering begin to end
-		previousEnd := begin
-		for _, node := range nodes {
-			b, e := node.Coverage()
-			if b != previousEnd {
-				t.Errorf("got=%d, want=%d", b, previousEnd)
-			}
-			previousEnd = e
-		}
-		if previousEnd != end {
-			t.Errorf("got=%d, want=%d", previousEnd, end)
+		if begin%201 == 14 {
+			t.Errorf("fail test immediately")
 		}
 	})
 }
